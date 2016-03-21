b13803c5f23288257a57c0dc10c82a860b489ba8
harn123 <harn@wp.pl>
2016-03-21 23:14:29 +0100

    Added files via upload

--- installcli.sh
+++ installcli.sh
@@ -11,14 +11,16 @@ echo "### start wget ###" >> $log
 wget -O /temp/get-pip.py https://bootstrap.pypa.io/get-pip.py >> $log
 echo "### start get-pip ###" >> $log
 python get-pip.py >> $log
-echo "### start upgrade gdg-httpsclient" >> $log
+echo "### start upgrade gdg-httpsclient ###" >> $log
 pip install --upgrade ndg-httpsclient >> $log
 #python get-pip.py >> $log
-echo "### start install CLI" >> $log
+echo "### start install CLI ###" >> $log
 pip install vca-cli >> $log
 wget -O /temp/deploy-vapp.sh  https://orbiteraimageseastus.blob.core.windows.net/esxi/deploy-vapp.sh >> $log
-/bin/bash /temp/deploy-vapp.sh >> $log
+echo "### start deployment ###" >> $log
+sudo /bin/bash /temp/deploy-vapp.sh 
 wget -O /etc/init.d/delete-vapp.sh https://orbiteraimageseastus.blob.core.windows.net/esxi/delete-vapp.sh >> $log
+echo "### set cleaning ###" >> $log
 chmod a+x /etc/init.d/delete-vapp.sh >> $log
 ln -s /etc/init.d/delete-vapp.sh /etc/rc0.d/K05deletevapp
 
